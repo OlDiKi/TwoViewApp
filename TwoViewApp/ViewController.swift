5dcdd657e7f44a727f6553e143f928f97e184679
OlDiKi <delorian101allgood@gmail.com>
2021-12-07 23:40:49 +0300

    Initial Commit

---
+++ TwoViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoViewApp
+//
+//  Created by Дмитрий Олейнер on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
